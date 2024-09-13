f647cc0847604d6a3e766b308d06385526312109
Thurain21 <thurainkosg@gmail.com>
2024-09-14 00:06:57 +0800

    adding components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Title";
+
+const Header = () => {
+  return (
+    <>
+      <Title data="Learning React js" />
+    </>
+  );
+};
+
+export default Header;
